55956a5291820f3a18c93d2cf0a67ba60e93a48c
31MartínS <mmsuquillo@espe.edu.ec>
2023-05-14 22:03:26 -0500

    Membrete

--- Templates 1.1/fraccion.h
+++ Templates 1.1/fraccion.h
@@ -1,3 +1,11 @@
+/*
+	Universidad de las Fuerzas Armadas ESPE
+	-- Estructura de Datos--
+	Alumno/s: Gutiérrez Miguel, Rivera Sebastián, Suquillo Martín
+	Fecha de creación: 10/05/2023
+	Enunciado: Sobrecarga de funciones y operadores
+	Nivel.- 3  NRC: 9671 
+*/
 #ifndef FRACCION_H
 #define FRACCION_H
 
