596ee4c1776b66fc08ad165c3a9c07d2b8053648
MGPOO <magutierrez9@espe.edu.ec>
2023-05-20 18:02:07 -0500

    Deber 4 avance

--- Deber Matrices memoria Din Parametrizado Template/Matriz.h
+++ Deber Matrices memoria Din Parametrizado Template/Matriz.h
@@ -1,14 +1,46 @@
+/*
+	Universidad de las Fuerzas Armadas ESPE
+	-- Estructura de Datos--
+	Alumno/s: Gutiérrez Miguel, Rivera Sebastián, Suquillo Martín
+	Fecha de creación: 20/05/2023
+	Enunciado: Matrices con Template
+	Nivel.- 3  NRC: 9671 
+*/
+
 #pragma once
 #include <iostream>
-class Matriz{
-	private:
-		int **matriz;
-		int dim;
-	public:
-		Matriz();
-		Matriz(int** ,int);
-		int getDim();
-		void setDim(int);
-		int **getMatriz();
-		void setMatriz(int **);
+
+template <typename T>
+class Matriz {
+private:
+    T** matriz;
+    int dim;
+public:
+    Matriz();
+    Matriz(T**, int);
+    int getDim();
+    void setDim(int);
+    T** getMatriz();
+    void setMatriz(T**);
 };
+
+template <typename T>
+	Matriz<T>::Matriz(T** m, int d) {
+    // Implementación del constructor
+}
+
+template <typename T>
+	int Matriz<T>::getDim() {
+}
+
+template <typename T>
+	void Matriz<T>::setDim(int d) {
+}
+
+template <typename T>
+	T** Matriz<T>::getMatriz() {
+}
+
+template <typename T>
+	void Matriz<T>::setMatriz(T** m) {
+}
