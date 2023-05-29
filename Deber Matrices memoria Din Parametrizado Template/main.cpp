77f524f953671750c9947169f7940ddc1d8ed43c
Sebastián Rivera <sbrivera2@espe.edu.ec>
2023-05-29 18:29:50 -0500

    Update main.cpp

--- Deber Matrices memoria Din Parametrizado Template/main.cpp
+++ Deber Matrices memoria Din Parametrizado Template/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "Matriz.h"
-#include "Operaciones.h"
-int ingresar(char *);
+#include "Matriz.cpp"
+#include "Operaciones.cpp"
+/*int ingresar(char *);
 int ingresar(char *msj){
 	int dim;
 	printf("\n%s\n",msj);
@@ -27,11 +27,44 @@ void iniciar(int  dim){
 	m2=operaciones.generar();
 	operaciones.imprimir(m1);
 	operaciones.imprimir(m2);*/
-}
+/*}*/
+/*
 int main(int argc, char** argv) {
 	int dim;
 	dim=ingresar("Ingrese la dimension de la matriz");
 	iniciar(dim);
 	
+	return 0;
+}*/
+
+
+template <typename T>
+Matriz<T> ::ingresar(char* msj) {
+	int dim;
+	printf("\n%s\n", msj);
+	scanf("%d", &dim);
+	
+	T** matriz = new T*[dim];
+	for (int i = 0; i < dim; i++) {
+		matriz[i] = new T[dim];
+	}
+	
+	return Matriz<T>(matriz, dim);
+}
+
+template <typename T>
+void iniciar(int dim) {
+	Matriz<T> matriz1 = ingresar<T>("Ingrese la matriz");
+	Operaciones<Matriz<T>> operaciones1(matriz1);
+	operaciones1.segmentar();
+	operaciones1.encerar();
+	operaciones1.imprimir();
+}
+
+int main(int argc, char** argv) {
+	int dim;
+	dim = ingresar<int>("Ingrese la dimension de la matriz");
+	iniciar<int>(dim);
+
 	return 0;
 }
