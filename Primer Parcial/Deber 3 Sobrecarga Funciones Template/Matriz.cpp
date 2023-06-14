a66c3f46e1d1bbb86a8dc3fd9a75c29c7890fab6
Sebastián Rivera <87491403+SbRivera@users.noreply.github.com>
2023-06-14 08:09:51 -0500

    Create Matriz.cpp

---
+++ Primer Parcial/Deber 3 Sobrecarga Funciones Template/Matriz.cpp
@@ -0,0 +1,32 @@
+/*
+	Universidad de las Fuerzas Armadas ESPE
+	-- Estructura de Datos--
+	Alumno/s: Gutiérrez Miguel, Rivera Sebastián, Suquillo Martín
+	Fecha de creación: 20/05/2023
+	Enunciado: Matrices con Template
+	Nivel.- 3  NRC: 9671 
+*/
+#include <iostream>
+#include "Matriz.h"
+
+
+template <typename T>
+	Matriz<T>::Matriz(T** m, int d) {
+    // Implementación del constructor
+}
+
+template <typename T>
+	int Matriz<T>::getDim() {
+}
+
+template <typename T>
+	void Matriz<T>::setDim(int d) {
+}
+
+template <typename T>
+	T** Matriz<T>::getMatriz() {
+}
+
+template <typename T>
+	void Matriz<T>::setMatriz(T** m) {
+}
