ed7560368e277e165c2b3f6d7f06ce77d0d59a16
Sebastián Rivera <87491403+SbRivera@users.noreply.github.com>
2023-06-14 08:10:16 -0500

    Create fraccion.h

---
+++ Primer Parcial/Deber 4 Multiplicación de Matrices/fraccion.h
@@ -0,0 +1,26 @@
+/*
+	Universidad de las Fuerzas Armadas ESPE
+	-- Estructura de Datos--
+	Alumno/s: Gutiérrez Miguel, Rivera Sebastián, Suquillo Martín
+	Fecha de creación: 22/05/2023
+	Enunciado: Multiplicacion de matrices
+	Nivel.- 3  NRC: 9671 
+*/
+
+#ifndef FRACCION_H
+#define FRACCION_H
+
+class fraccion {
+private:
+    int numerador;
+    int denominador;
+
+public:
+    fraccion(int numerador = 0, int denominador = 1);
+    int getNumerador() const;
+    int getDenominador() const;
+    void simplificar();
+    void imprimir() const;
+};
+
+#endif //FRACCION_H
