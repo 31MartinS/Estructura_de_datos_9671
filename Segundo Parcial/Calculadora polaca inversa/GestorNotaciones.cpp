419af075eaa1ca3e0e941c069e50c487b0eb89a8
Sebastián Rivera <sbrivera2@espe.edu.ec>
2023-06-26 06:08:08 -0500

    Create GestorNotaciones.cpp

---
+++ Segundo Parcial/Calculadora polaca inversa/GestorNotaciones.cpp
@@ -0,0 +1,19 @@
+/*
+	Universidad de las Fuerzas Armadas ESPE
+	-- Estructura de Datos--
+	Alumno/s: Gutiérrez Miguel, Rivera Sebastián, Suquillo Martín
+	Enunciado: Deber Calculadora Polaca
+	Nivel.- 3  NRC: 9671 
+*/
+
+#include "Menu.cpp"
+#include <iostream>
+using namespace std;
+int main() {
+	Menu objM;
+	objM.menuPrincipal();
+ 
+    
+
+    return 0;
+}
