af05a5c7911ae3574695b76f40528e4d73b64045
Sebastián Rivera <sbrivera2@espe.edu.ec>
2023-06-26 06:08:20 -0500

    Create Menu.h

---
+++ Segundo Parcial/Calculadora polaca inversa/Menu.h
@@ -0,0 +1,22 @@
+/*
+	Universidad de las Fuerzas Armadas ESPE
+	-- Estructura de Datos--
+	Alumno/s: Gutiérrez Miguel, Rivera Sebastián, Suquillo Martín
+	Enunciado: Deber Calculadora Polaca
+	Nivel.- 3  NRC: 9671 
+*/
+
+ #include <iostream>
+#include <cstdlib>
+#include <conio.h>
+
+#include <windows.h>
+using namespace std;
+class Menu{
+	public:
+		void menuPrincipal();
+		int menu(const char *titulo,const char *opciones[],int n);
+		void gotoxy(int x,int y);
+		//int validarCedula(char *cedulaC);
+		static bool verificarCedula(const string &cedula);
+};
